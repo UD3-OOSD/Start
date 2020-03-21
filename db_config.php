50d7d74ad9387424118ce1896c116d254cf12679
DevinDeSilva <57395782+DevinDeSilva@users.noreply.github.com>
2020-03-21 16:39:47 +0530

    Revert "backend on code branch"

--- db_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  //connect to SQLIitaDatabase
-  $conn = mysqli_connect('localhost','nipdep','test1234','Project_1');
-
-  if(!$conn){
-    echo 'Connection error:'.mysqli_connect_error;
-  }
